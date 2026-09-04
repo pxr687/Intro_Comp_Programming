2c1076df9712873c5526ca2933a68e4688ccb478
pxr687 <pxr_687@outlook.com>
2026-09-04 20:21:52 +0700

    update exercise links (and script to make them) with workaround to get custom modules onto Google Colab.

--- mk_launch_links.py
+++ mk_launch_links.py
@@ -3,19 +3,28 @@
 # Example usage:
 
 # from mk_launch_links import make_launch_links
-# make_launch_links(4, "Cool Stuff", "exercise_10_cool_stuff.ipynb")
+# make_launch_links(4, "Cool Stuff", "exercise_10_cool_stuff.ipynb", "mark_exercise_10.py")
 
-def make_launch_links(ex_num, ex_name, notebook_link):
+def mk_launch_links(ex_num, ex_name, notebook_link, name_of_exercise_marking_file):
     text =  f"""
-    ## Exercise {ex_num} - {ex_name}
+## Exercise {ex_num} - {ex_name}
 
-    We will now do our another exercise!
+We will now do another exercise!
 
-    *Remember*: your [assignments, tests and exams](https://pxr687.github.io/Intro_Comp_Programming/1_02_Class_Grades_Assignments_Exams.html) will also be a bit like this.
+*Remember*: your [assignments, tests and exams](https://pxr687.github.io/Intro_Comp_Programming/1_02_Class_Grades_Assignments_Exams.html) will also be a bit like this.
 
-    Click [here 🚀](https://mybinder.org/v2/gh/pxr687/Intro_Comp_Programming/main?urlpath=tree/{notebook_link}) to launch the exercise on Binder.
+Click [here 🚀](https://mybinder.org/v2/gh/pxr687/Intro_Comp_Programming/main?urlpath=tree/{notebook_link}) to launch the exercise on Binder.
 
-    Or click [here 🚀](https://colab.research.google.com/github/pxr687/Intro_Comp_Programming/blob/main/{notebook_link}) to launch the exercise on Google Colab.
+Or click [here 🚀](https://colab.research.google.com/github/pxr687/Intro_Comp_Programming/blob/main/{notebook_link}) to launch the exercise on Google Colab.
+
+**NOTE**: if you use Google Colab, you will have to make a new cell at the start of the notebook, and run this code:
+
+```
+!wget -q https://raw.githubusercontent.com/pxr687/Intro_Comp_Programming/main/{name_of_exercise_marking_file}
+!wget -q https://raw.githubusercontent.com/pxr687/Intro_Comp_Programming/main/marking_sub_functions.py
+!wget -q https://raw.githubusercontent.com/pxr687/Intro_Comp_Programming/main/mk_var.py
+!pip install jupyprint
+```
     """
 
     print(text)
