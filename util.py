854a2277762256641ecfcd7c7a63559f5e0c17e4
pxr687 <pxr_687@outlook.com>
2026-09-05 19:45:31 +0700

    operation obs

---
+++ util.py
@@ -0,0 +1,9 @@
+# Random array entries.
+import numpy as np
+
+def mk_t_1():
+    rndm = np.array([-1*10*(100 * 22)/10*-1, "3S5I!AyM_ HUJN_I*V^E=RRSyIYTUYP"[1::2] ,
+                      (27*1000) + (1000/27) - (27*1000),
+                    "fTthyiusi iioso oat rfeiwnaez aacnvsbwyetrw"[1::2],
+                      999 * 3 + (100*100*2) - 999 * 3])
+    return rndm
